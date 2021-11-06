af4d69d19cde7ea40769ddab45b5a9c9e80bbb54
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2021-11-06 20:48:53 +0530

    updated contract as per requirements

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -9,45 +9,113 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Vesting is Ownable, ReentrancyGuard {
-    using Counters for Counters.Counter;
     using SafeMath for uint256;
 
-    constructor(address _token) {}
+    address public stableCoin;
 
-    struct TierInfo {
-        uint256 maxTokens;
+    constructor(address _stableCoin) {
+        stableCoin = _stableCoin;
+    }
+
+    struct PreSaleTierInfo {
+        uint256 maxTokensPerWallet;
         uint256 startTime;
-        uint256 tokensReleased;
+        uint256 endTime;
+        uint256 maxTokensForTier;
+        uint256 price;
     }
 
-    mapping(address => TierInfo) public tierInfo;
+    mapping(uint256 => uint256) public startVestingForTier;
+    mapping(uint256 => mapping(uint256 => uint256)) public allocationPerMonth;
+    mapping(address => mapping(uint256 => uint256)) public tokensBought;
+    mapping(uint256 => uint256) public totalTokensBoughtForTier;
 
-    function setTierInfo(
-        address _address,
-        uint256 _maxTokens,
-        uint256 _startTime
+    TierInfo[] public tierInfo;
+
+    function createPreSaleTier(
+        uint256 _maxTokensPerWallet,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _maxTokensForTier,
+        uint256 _price
+    ) external onlyOwner {
+        tierInfo.push(
+            TierInfo({
+                maxTokensPerWallet: _maxTokensPerWallet,
+                startTime: _startTime,
+                endTime: _endTime,
+                price: _price,
+                maxTokensForTier: _maxTokensForTier
+            })
+        );
+    }
+
+    function updatePreSaleTier(
+        uint256 _tierId,
+        uint256 _maxTokensPerWallet,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _maxTokensForTier,
+        uint256 _price
+    ) external onlyOwner {
+        require(
+            tierInfo[_tierId].startTime < block.timestamp,
+            "Pre sale not yet started"
+        );
+        tierInfo[_tierId].maxTokensPerWallet = _maxTokensPerWallet;
+        tierInfo[_tierId].startTime = _startTime;
+        tierInfo[_tierId].endTime = _endTime;
+        tierInfo[_tierId].maxTokensForTier = _maxTokensForTier;
+        tierInfo[_tierId].price = _price;
+    }
+
+    function tierLength() external view returns (uint256) {
+        return tierInfo.length;
+    }
+
+    function setVestingForTier(uint256 _tierId, uint256 _startTime)
+        public
+        onlyOwner
+    {
+        require(_tierId < tierInfo.length, "Invalid tier id");
+        require(
+            tierInfo[_tierId].startTime < _startTime,
+            "Tier not yet started"
+        );
+        startVestingForTier[_tierId] = _startTime;
+    }
+
+    function setAllocation(
+        uint256 _tierId,
+        uint256 _month,
+        uint256 _allocation
     ) public onlyOwner {
-        require(
-            tierInfo[_address].startTime < block.timestamp,
-            "Tier Already Passed"
-        );
-        tierInfo[_address].maxTokens = _maxTokens;
-        tierInfo[_address].startTime = _startTime;
+        require(_month > 0, "Invalid month number");
+        require(_month < 13, "Invalid month number");
+        require(_tierId < tierInfo.length, "Invalid tier id");
+        allocationPerMonth[_tierId][_month] = _allocation;
     }
 
-    function releaseToken(uint256 _amount) public onlyOwner {
-        uint256 tokensReleased = tierInfo[msg.sender].tokensReleased;
+    function buyTokens(uint256 _tierId, uint256 _numTokens) public payable {
         require(
-            block.timestamp > tierInfo[msg.sender].startTime,
-            "Vesting not started yet"
+            tierInfo[_tierId].startTime < block.timestamp,
+            "Pre sale not yet started"
+        );
+        require(tierInfo[_tierId].endTime > block.timestamp, "Pre sale over");
+        require(
+            totalTokensBoughtForTier[_tierId] + _numTokens <
+                tierInfo[_tierId].maxTokensForTier,
+            "Cant buy more tokens for this tier"
         );
         require(
-            tokensReleased + _amount < tierInfo[msg.sender].maxTokens,
-            "Insufficient balance"
+            tokensBought[msg.sender] + _numTokens <
+                tierInfo[_tierId].maxTokensPerWallet,
+            "You cant buy more tokens"
         );
-        IERC20(token).transferFrom(address(this), address(msg.sender), _amount);
-        tierInfo[msg.sender].tokensReleased = tierInfo[msg.sender]
-            .tokensReleased
-            .add(_amount);
+        uint256 tokenPrice = tierInfo[_tierId].price.mul(_numTokens);
+        IERC20(stableCoin).transferFrom(msg.sender, address(this), _amount);
+        tokensBought[msg.sender] = tokensBought[msg.sender].add(_numTokens);
+        totalTokensBoughtForTier[_tierId] = totalTokensBoughtForTier[_tierId]
+            .add(_numTokens);
     }
 }
